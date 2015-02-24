7e27f140c1fb820afb17bdde1545c9e27825b16d
Dominik Heimstädt <dominik.heimstaedt@gmail.com>
2015-02-24 18:06:37 +0100

    Extended -m option description

--- src/main/java/com/github/skare69/boo/DuplicateFileFinder.java
+++ src/main/java/com/github/skare69/boo/DuplicateFileFinder.java
@@ -295,7 +295,7 @@ public class DuplicateFileFinder
         DIRECTORY_TO_SCAN("t", "target", true, "target directory to scan", true),
         FLAT_SCAN("f", "flat", false, "do a flat scan of the provided directory only; i.e. no recursion (default: false)"),
         VERBOSE_OUTPUT("v", "verbose", false, "print a verbose output of what's happening"),
-        MAX_FILE_SIZE("m", "max", true, "the maximum size of a file to scan (default 5 MB)");
+        MAX_FILE_SIZE("m", "max", true, "the maximum size of a file to scan in bytes (default 5 MB)");
 
         private String opt;
         private String longOpt;
